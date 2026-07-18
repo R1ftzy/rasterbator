ee1e6caa8d0eb32817dc2cbf2a097102a7cac675
R1ftzy <r1ftzyplays@gmail.com>
2026-07-19 00:24:03 +0530

    lowered resolution

--- include/config.h
+++ include/config.h
@@ -1,8 +1,8 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#define SCREEN_WIDTH 1280
-#define SCREEN_HEIGHT 720
+#define SCREEN_WIDTH 960
+#define SCREEN_HEIGHT 576
 #define FNEAR 0.1f
 #define FFAR 1000.0f
 #define FFOV 90.0f
