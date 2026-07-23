fbbd95a0bf294ea856fe897f2286ead161a3098d
R1ftzy <r1ftzyplays@gmail.com>
2026-07-24 00:54:21 +0530

    Fix function name casing for rgba and RGBA in color.h and color.c

--- include/color.h
+++ include/color.h
@@ -3,23 +3,22 @@
 
 #include <stdint.h>
 
-#define WHITE RGBA(255, 255, 255, 255)
-#define BLACK RGBA(0, 0, 0, 255)
-#define RED RGBA(255, 0, 0, 255)
-#define GREEN RGBA(0, 255, 0, 255)
-#define BLUE RGBA(0, 0, 255, 255)
-#define YELLOW RGBA(255, 255, 0, 255)
-#define CYAN RGBA(0, 255, 255, 255)
-#define MAGENTA RGBA(255, 0, 255, 255)
-#define PINK RGBA(255, 105, 180, 255)
-#define ORANGE RGBA(255, 165, 0, 255)
-#define PURPLE RGBA(128, 0, 128, 255)
-#define TEAL RGBA(0, 128, 128, 255)
-#define LIME RGBA(50, 205, 50, 255)
-#define CORAL RGBA(255, 127, 80, 255)
+#define WHITE rgba(255, 255, 255, 255)
+#define BLACK rgba(0, 0, 0, 255)
+#define RED rgba(255, 0, 0, 255)
+#define GREEN rgba(0, 255, 0, 255)
+#define BLUE rgba(0, 0, 255, 255)
+#define YELLOW rgba(255, 255, 0, 255)
+#define CYAN rgba(0, 255, 255, 255)
+#define MAGENTA rgba(255, 0, 255, 255)
+#define PINK rgba(255, 105, 180, 255)
+#define ORANGE rgba(255, 165, 0, 255)
+#define PURPLE rgba(128, 0, 128, 255)
+#define TEAL rgba(0, 128, 128, 255)
+#define LIME rgba(50, 205, 50, 255)
+#define CORAL rgba(255, 127, 80, 255)
 
-
-uint32_t RGBA(int r, int g, int b, int a);
+uint32_t rgba(int r, int g, int b, int a);
 uint32_t rgb(int r, int g, int b);
 uint32_t HSV(float h, float s, float v);
 #endif
--- src/color.c
+++ src/color.c
@@ -1,6 +1,6 @@
 #include <stdint.h>
 
-uint32_t RGBA(int r, int g, int b, int a)
+uint32_t rgba(int r, int g, int b, int a)
 {
   uint32_t argb = (a << 24) | (r << 16) | (g << 8) | b;
   return argb;
